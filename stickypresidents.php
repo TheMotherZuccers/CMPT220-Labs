0e7866d37018be85b794d94827fec270e455d5ab
WilliamKluge <klugewilliam@gmail.com>
2018-10-31 15:38:20 -0400

    Lab 8

---
+++ stickypresidents.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Style-Type" content="text/css"/>
+    <title>sticky</title>
+    <link href="/library/skin/tool_base.css" type="text/css" rel="stylesheet" media="all"/>
+    <link href="/library/skin/morpheus-default/tool.css" type="text/css" rel="stylesheet" media="all"/>
+    <script type="text/javascript" src="/library/js/headscripts.js"></script>
+    <style>body {
+            padding: 5px !important;
+        }</style>
+</head>
+<body>
+<!--
+This PHP script was modified based on result.php in McGrath (2012).
+It demonstrates how to ...
+  1) Connect to MySQL.
+  2) Write a complex query.
+  3) Format the results into an HTML table.
+  4) Update MySQL with form input.
+By Ron Coleman
+-->
+<!DOCTYPE html>
+<html>
+<?php
+# If user opened the page without submitting, initialize the fields
+if ( $_SERVER[ 'REQUEST_METHOD' ] == 'GET' ) {
+    $num = "" ;
+    $fname = "" ;
+    $lname = "";
+}
+
+# Otherwise, user submitted the form, so let's validate
+else if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' )
+{
+    # Initialize an error array.
+    $errors = array();
+
+    $num = $_POST[ 'num' ] ;
+    $fname = $_POST[ 'fname' ] ;
+    $lname = $_POST[ 'lname' ] ;
+
+    # Check for a name & email address.
+    if ( empty( $num ) )  {
+        $errors[] = 'num' ;
+    }
+    else {
+        $num = trim( $num )  ;
+    }
+
+    if ( empty( $_POST[ 'fname' ] ) ) {
+        $errors[] = 'fname' ;
+    }
+    else {
+        $fname = trim( $fname )  ;
+    }
+
+    if ( empty( $_POST[ 'lname' ] ) ) {
+        $errors[] = 'lname' ;
+    }
+    else {
+        $lname = trim( $lname )  ;
+    }
+
+    # Report result.
+    if( !empty( $errors ) )
+    {
+        echo 'Error! Please enter your  ' ;
+        foreach ( $errors as $field ) { echo " - $field " ; }
+    }
+    else {
+        echo "<p>Success! Thanks $num </p>" ;
+    }
+}
+
+# Show the input form with whatever we got for fields
+show_form($num,$fname,$lname) ;
+
+# Shows the input form
+function show_form($num,$fname,$lname) {
+    echo '<form action="stickypresidents.php" method="POST">' ;
+    echo '<p>Number: <input type="text" name="num" value="' . $num . '"> </p> ' ;
+    echo '<p>First Name: <input type="text" name="fname" value="' . $fname . '"></p>' ;
+    echo '<p>Last Name: <input type="text" name="lname" value="' . $lname . '"></p>' ;
+    echo '<p><input type="submit"></p>' ;
+    echo '</form>' ;
+}
+?>
+</html>
